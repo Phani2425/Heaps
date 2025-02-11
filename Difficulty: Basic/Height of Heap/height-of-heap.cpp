b24648e582d3bfa040dc05906fa7cdd8f43117e5
Phani Bhusan Mohanty <mohantyphanibhusan@gmail.com>
2025-02-12 00:14:22 +0530

    Update height-of-heap.cpp

--- Difficulty: Basic/Height of Heap/height-of-heap.cpp
+++ Difficulty: Basic/Height of Heap/height-of-heap.cpp
@@ -17,24 +17,24 @@ using namespace std;
 // TC:- O(n)
 // SC:- O(logn)
 
-// class Solution{
-// public:
+class Solution{
+public:
 
-//     int helperFun(int index,int N){
-//         if(index*2+1>=N){
-//             return 0;
-//         }
+    int helperFun(int index,int N){
+        if(index*2+1>=N){
+            return 0;
+        }
         
-//         int left = helperFun(index*2+1,N);
-//         int right = helperFun(index*2+2,N);
+        int left = helperFun(index*2+1,N);
+        int right = helperFun(index*2+2,N);
         
-//         return  1+max(left,right);
-//     }
+        return  1+max(left,right);
+    }
 
-//     int heapHeight(int N, int arr[]){
-//       return helperFun(0,N);
-//     }
-// };
+    int heapHeight(int N, int arr[]){
+      return helperFun(0,N);
+    }
+};
 
 // way - 2
 
@@ -68,6 +68,49 @@ public:
       
     }
 };
+
+
+
+// WAY - 3
+
+// isme to maja hi agaya bhai... ese kaise soch sakte hein log yaar
+// bohot hu intutive and obseravtional solution tha ye wala
+
+// dekh imagine kar ek heap au start numbering the nodes from 1 and in left to right order
+// then to dekhibu each level will start from a node numbered as (2^levelCount) and again the next level will
+// start form a node numbered as (2^levelCount+1)
+
+// au gote katha ethirru figure out heijuachi je :- if we devide a number >= 2^x and < 2^(x+1) then we will reach to 1 after deviding it x times
+
+// so a amne hauchi ame jadi tottal count janithiba au taku ame 2 re devide karichaliba untill we reach to 1 then the no of times we will devide it will be the height of the heap
+
+// bhai saab kn sountion thila yaar
+
+// TC:- O(logN)
+// SC:-O(1)
+
+class Solution{
+    
+    public:
+    int heapHeight(int N,int arr[]){
+        // handling edge case
+        if(N == 1){
+            return 1;
+        }
+        
+        int count = 0;
+        
+        while(N/2 != 0){
+            N = N/2;
+            count++;
+        }
+        
+        return count;
+        
+    }
+};
+
+
 
 
 //{ Driver Code Starts.
