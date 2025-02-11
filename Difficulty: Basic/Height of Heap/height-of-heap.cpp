f596617abb9a693d163f419d0c2887a1ca7eab6b
Phani Bhusan Mohanty <mohantyphanibhusan@gmail.com>
2025-02-12 00:00:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Height of Heap/height-of-heap.cpp
+++ Difficulty: Basic/Height of Heap/height-of-heap.cpp
@@ -7,24 +7,68 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
+// WAY - 1
+
+//complete brute force approach and quite simmilar approach to finding the height of the normal tree
+// there i was going to left and right child using the links and here i am just doing thet using indexes as i know 
+// that as this is arary representation of heap hence there is no chance that a NULL can come 
+// in between array as heap is a complete binary tree
+
+// TC:- O(n)
+// SC:- O(logn)
+
+// class Solution{
+// public:
+
+//     int helperFun(int index,int N){
+//         if(index*2+1>=N){
+//             return 0;
+//         }
+        
+//         int left = helperFun(index*2+1,N);
+//         int right = helperFun(index*2+2,N);
+        
+//         return  1+max(left,right);
+//     }
+
+//     int heapHeight(int N, int arr[]){
+//       return helperFun(0,N);
+//     }
+// };
+
+// way - 2
+
+//mu bhabili ki are bhai jadi eta heap i.e a complete binary tree then ara feature hauchi ki
+// all level will be completely filled except the last level i which the nodes will be as left  as possible 
+
+// and this means in the last level even if it have a single node still it will be in the left most position
+// so from root if we go only left and each time we go we will add 1 to a variable then for sure we will get the height of the tree
+
+
+
 class Solution{
 public:
 
-    int helperFun(int index,int arr[],int N){
-        if(index*2+1>=N){
-            return 0;
+    int heapHeight(int N, int arr[]){
+        
+        // handling a edge case
+        if(N==1){
+            return 1;
         }
         
-        int left = helperFun(index*2+1,arr,N);
-        int right = helperFun(index*2+2,arr,N);
+        int count = 0;
+        int index = 0;
         
-        return  1+max(left,right);
-    }
-
-    int heapHeight(int N, int arr[]){
-       return helperFun(0,arr,N);
+        while(index*2 + 1 < N){
+            index = index*2+1;
+            count++;
+        }
+        
+        return count;
+      
     }
 };
+
 
 //{ Driver Code Starts.
 
